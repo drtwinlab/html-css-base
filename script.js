a5f49c63c9b807df703a51f295fc1c870f8836a5
drtwinlab <56961525+drtwinlab@users.noreply.github.com>
2025-11-28 17:19:06 +0100

    new update

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+let zname = "Admin";
 
+
+if (zname === "Admin") {
+  let pass = "TheMaster";
+            if (pass === "TheMaster") {
+                console.log("Welcome")
+            } else if (pass === "" || pass === null){console.log("Canceled")}
+            else {console.log("Wrong password")}
+
+} else if (zname === "" || zname === null) {
+    console.log("Cancelled")
+} else {console.log("Idk")}
