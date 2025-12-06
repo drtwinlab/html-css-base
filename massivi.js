464ccdbf4d5961463531e4cf790dc960387f2805
drtwinlab <56961525+drtwinlab@users.noreply.github.com>
2025-12-06 17:35:45 +0100

    Yo

---
+++ massivi.js
@@ -0,0 +1,26 @@
+let customers = [
+  { cname: "Rost", purchases: [10, 20, 30] },
+  { cname: "Maria", purchases: [5, 5, 5] },
+  { cname: "Jon", purchases: [100, 50] }
+];
+
+function taxesMultiply(tax) {
+    return Number((tax * 1.19).toFixed(2));
+}
+
+function customersTaxCount(customer) {
+let totals = 0;
+for (let el of customer.purchases.map(taxesMultiply)){
+totals = el + totals;
+}
+    return {
+        cname : customer.cname, 
+        purchsesWithTax: customer.purchases.map(taxesMultiply),
+        total: totals,
+    }
+}
+
+
+
+let customersTaxed = customers.map(customersTaxCount);
+console.log(customersTaxed)
